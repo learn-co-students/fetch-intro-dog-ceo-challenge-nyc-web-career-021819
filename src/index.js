cc0f8a8bf280c30497c892bc1c0c73596028bacb
dlee16 <diana.lee116@gmail.com>
2019-04-04 21:46:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log('%c HI', 'color: firebrick')
+function fetchDogs(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(response => response.json())
+        .then(json => json.message.forEach(function(dog){
+           let main = document.querySelector('#dog-image-container')
+           let img = document.createElement("img")
+           img.src = dog
+           main.appendChild(img)
+        })
+    );
+}
+
+function fetchBreeds(){
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then(json => Object.keys(json.message).forEach(function(dog){
+            let main = document.querySelector('#dog-breeds')
+            let breed = document.createElement("li")
+            breed.innerText = dog
+            let i = 0
+            while (i < document.getElementsByTagName("li").length) {
+                ++i
+            }
+            breed.setAttribute("id", i)
+            breed.addEventListener("click", function(){
+                breed.style.color = "purple"
+            })
+            main.appendChild(breed);
+        }))
+
+}
+
+function filterDogs(){
+    const dropdown = document.querySelector("#breed-dropdown")
+    dropdown.addEventListener("change", function(event){
+        const value = event.target.value
+        const list = document.querySelectorAll("li")
+        list.forEach(function(list){
+            if (list.innerText[0] === value) {
+            return list.style.display = "list-item"
+            }
+            else {
+            return list.style.display = "hidden"
+            }
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchDogs()
+    fetchBreeds()
+    filterDogs()
+    })
